e4a2786ce897e3a8b705744e021e7879846c6286
中野大輝 <nakanodaiki@nakanodaikinoMacBook-Air.local>
2019-10-07 21:41:56 +0900

    create user model

---
+++ db/migrate/20191007124015_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :age
+      t.string :interger
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
