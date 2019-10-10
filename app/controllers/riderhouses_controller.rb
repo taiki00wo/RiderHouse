8b6727330cd23c5172dc5750e13f8166a3d61d7f
中野大輝 <nakanodaiki@nakanodaikinoMacBook-Air.local>
2019-10-10 21:08:36 +0900

    riderhouse_controller

--- app/controllers/riderhouses_controller.rb
+++ app/controllers/riderhouses_controller.rb
@@ -8,18 +8,48 @@ class RiderhousesController < ApplicationController
   end
 
   def new
+    # logger.debug ("newに入りました")
     @riderhouse = Riderhouse.new
   end
 
   def create
+    # logger.debug ("createに入りました")
+    @riderhouse = Riderhouse.new(riderhouse_params)
+    if @riderhouse.save
+      flash[:notice] = "ライダーハウス：#{@riderhouse.name}を登録しました。"
+      redirect_to @riderhouse
+    else 
+      render 'new'
+    end
   end
 
   def edit
+    @riderhouse = Riderhouse.find(params[:id])
   end
 
   def update
+    @riderhouse = Riderhouse.find(params[:id])
+    if @riderhouse.update(riderhouse_params)
+      flash[:notice] = "ライダーハウス：#{@riderhouse.name}を更新しました。"
+      redirect_to @riderhouse
+    else 
+      render 'edit'
+    end
   end
 
   def destroy
+    @riderhouse = Riderhouse.find(params[:id])
+    if @riderhouse.destroy
+      flash[:notice] = "ライダーハウス：#{@riderhouse.name}を削除しました。"
+      redirect_to riderhouses_url
+    else 
+      render 'edit'
+    end
+  end
+
+  private
+
+  def riderhouse_params
+    params.require(:riderhouse).permit(:name, :address, :tel, :price, :opening_hours)
   end
 end
