7f22da09fa0cad3e3ce96223784aad335358bcd9
中野大輝 <nakanodaiki@nakanodaikinoMacBook-Air.local>
2019-11-02 20:05:29 +0900

    byebug

--- app/controllers/riderhouses_controller.rb
+++ app/controllers/riderhouses_controller.rb
@@ -4,7 +4,6 @@ class RiderhousesController < ApplicationController
     gon.riderhouses = @riderhouses
     @API_KEY_map = ENV['API_KEY_map']
     gon.API_KEY_weather = ENV['API_KEY_weather']
-    byebug
     @riderhouses = Riderhouse.page(params[:page]).per(8)
   end
 
