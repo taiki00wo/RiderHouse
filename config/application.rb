e7d17223c179dc5b97664c6ed36e582eec538284
中野大輝 <nakanodaiki@nakanodaikinoMacBook-Air.local>
2019-11-09 15:59:28 +0900

    heroku update

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RiderHouse
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 追記
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -25,5 +28,3 @@ module RiderHouse
     end
   end
 end
-
-config.assets.initialize_on_precompile = false
