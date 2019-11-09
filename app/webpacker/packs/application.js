11af761ea64617c77ab08be0e7e5baa8c71a292a
中野大輝 <nakanodaiki@nakanodaikinoMacBook-Air.local>
2019-11-09 21:44:02 +0900

    change webpack

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -3,19 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap'
-import '../src/application.scss'
-
+require('bootstrap')
 require('jquery')
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 require('popper.js')
-require('../src/openweathermap.js')
-require('../src/googlemap.js')
 
+require('../stylesheet/application')
+require('../stylesheet/custom')
+require('../javascript/openweathermap')
+require('../javascript/googlemap')
+
+// require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
